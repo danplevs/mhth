05c75df69a36fba12b4c03597911b60821960b86
danpl <daniel.re4@gmail.com>
2022-02-07 01:13:42 +0000

    add app factory

--- mhth.py
+++ mhth.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 import json
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def index():
-    with open("./data/DHT11_l.json") as file:
-        temperature, humidity = json.load(file).values()
-    return render_template("index.html", temperature=temperature, humidity=humidity)
+    @app.route("/")
+    def index():
+        with open("./data/DHT11_l.json") as file:
+            temperature, humidity = json.load(file).values()
+        return render_template("index.html", temperature=temperature, humidity=humidity)
 
-if __name__ == "__main__":
-    app.run()
+    return app
